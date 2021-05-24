824c47c41c662c5c243f4edea9f46e6dc62ccd08
Amaury <ricardo901130@gmail.com>
2021-05-24 17:01:04 -0300

    refactor dimens

--- core/theme/lib/variants/light.dart
+++ core/theme/lib/variants/light.dart
@@ -1,8 +1,5 @@
-import 'package:theme/resources/colors.dart';
 import 'package:theme/resources/colors_light.dart';
-import 'package:theme/spacing.dart';
 import 'package:theme/theme.dart';
-import 'package:flutter/material.dart';
 
 class LightTheme extends LocalTheme {
   LightTheme() : super(colors: ColorsLight());
